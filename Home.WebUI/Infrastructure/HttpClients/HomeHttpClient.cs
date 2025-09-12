0843142db0087ec4ac767d10a61bdddd8e1237c4
Heals26 <mitch_healy@outlook.com>
2025-09-12 19:33:13 +1000

    Throw not implemented for http clients

--- Home.WebUI/Infrastructure/HttpClients/HomeHttpClient.cs
+++ Home.WebUI/Infrastructure/HttpClients/HomeHttpClient.cs
@@ -1,5 +1,6 @@
 ﻿using Home.WebUI.Infrastructure.ApiProviders.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace Home.WebUI.Infrastructure.HttpClients;
 
@@ -10,7 +11,7 @@ public class HomeHttpClient(HttpClient httpClient)
 
     public async Task<TResponse> SendAsync<TRequest, TResponse>(TRequest request, ApiProviderHelper apiProvider, Action<ProblemDetails> errors, CancellationToken cancellationToken)
     {
-
+        throw new NotImplementedException();
     }
 
     private async Task<TResponse> SendRequestAsync<TResponse>(HttpRequestMessage httpMessage, Action<ProblemDetails> errors, CancellationToken cancellationToken)
@@ -20,6 +21,10 @@ public class HomeHttpClient(HttpClient httpClient)
             var _HttpResponse = await httpClient.SendAsync(httpMessage, HttpCompletionOption.ResponseContentRead, cancellationToken);
             var _Content = await _HttpResponse.Content.ReadAsStringAsync();
 
+            if (_HttpResponse.IsSuccessStatusCode)
+                return JsonSerializer.Deserialize<TResponse>(_Content);
+
+            errors.Invoke(JsonSerializer.Deserialize<ProblemDetails>(_Content));
         }
         catch (HttpRequestException)
         {
@@ -29,6 +34,8 @@ public class HomeHttpClient(HttpClient httpClient)
         {
 
         }
+
+        throw new NotImplementedException();
     }
 
     #endregion Methods
