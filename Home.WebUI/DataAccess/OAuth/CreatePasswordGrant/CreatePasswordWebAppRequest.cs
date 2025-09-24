6825ac898001559b2058fdf432f397c26f919bf2
Heals26 <mitch_healy@outlook.com>
2025-09-24 19:30:49 +1000

    modify oauth requests

--- Home.WebUI/DataAccess/OAuth/CreatePasswordGrant/CreatePasswordWebAppRequest.cs
+++ Home.WebUI/DataAccess/OAuth/CreatePasswordGrant/CreatePasswordWebAppRequest.cs
@@ -1,38 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
-namespace Home.WebUI.DataAccess.OAuth;
+namespace Home.WebUI.DataAccess.OAuth.CreatePasswordGrant;
 
-public class OAuthWebAppRequest
+public class CreatePasswordWebAppRequest
 {
 
     #region Properties
 
-    [FromForm(Name = "client_id")]
     [JsonPropertyName("client_id")]
     public long ClientID { get; set; }
 
-    [FromForm(Name = "client_secret")]
     [JsonPropertyName("client_secret")]
     public string ClientSecret { get; set; }
 
-    [FromForm(Name = "grant_type")]
     [JsonPropertyName("grant_type")]
     public string GrantType { get; set; }
 
-    [FromForm(Name = "password")]
     [JsonPropertyName("password")]
     public string Password { get; set; }
 
-    [FromForm(Name = "refresh_token")]
-    [JsonPropertyName("refresh_token")]
-    public string RefreshToken { get; set; }
-
-    [FromForm(Name = "scope")]
     [JsonPropertyName("scope")]
     public string Scope { get; set; }
 
-    [FromForm(Name = "username")]
     [JsonPropertyName("username")]
     public string Username { get; set; }
 
---
+++ Home.WebUI/DataAccess/OAuth/CreateRefreshGrant/CreateRefreshGrantWebAppRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Home.WebUI.DataAccess.OAuth.CreateRefreshGrant;
+
+public class CreateRefreshGrantWebAppRequest
+{
+
+    #region Properties
+
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; }
+
+    [JsonPropertyName("client_id")]
+    public long ClientID { get; set; }
+
+    [JsonPropertyName("client_secret")]
+    public string ClientSecret { get; set; }
+
+    [JsonPropertyName("grant_type")]
+    public string GrantType { get; set; }
+
+    #endregion Properties
+
+}
